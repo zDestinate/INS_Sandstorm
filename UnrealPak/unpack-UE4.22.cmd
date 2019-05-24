7a7ce8dc663c3628079efdb33a1fe8e85bffb6bf
zWolfi <EA.ADM1N.one@gmail.com>
2019-05-24 17:57:12 -0400

    Add files via upload

--- UnrealPak/unpack-UE4.22.cmd
+++ UnrealPak/unpack-UE4.22.cmd
@@ -1,5 +1,5 @@
 for %%s in (*.pak) do (
-	Core\UE4\UnrealPak\UnrealPak.exe %1 -list > %%s.txt
-	Core\UE4\UnrealPak\UnrealPak.exe %1 -extract ../../../unpacked/
+	Core\UE4\UnrealPak\UnrealPak.exe %%s -list > %%s.txt
+	Core\UE4\UnrealPak\UnrealPak.exe %%s -extract ../../../unpacked/
 )
 pause
