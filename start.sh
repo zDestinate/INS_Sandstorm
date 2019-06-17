3c47a6cbebc3b545680c65dff8a75675f9bb410e
zWolfi <EA.ADM1N.one@gmail.com>
2019-06-17 19:26:43 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -50,4 +50,4 @@ RANDOM=$$$(date +%N)
 #set map
 strMap=${strMapList[$RANDOM % ${#strMapList[@]}]}
 
-./Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping $strMap?port=27102?queryport=27131?MaxPlayers=20?game=$strGameMode -log -AdminList=Admins -MapCycle=MapCycle -ruleset=$strRuleSet -ApiKey=XXXXXXXXXXXXXXXXXXXXXXXX -GameStats -EnableCheats -NoEAC
+./Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping $strMap?port=27102?queryport=27131?MaxPlayers=20?game=$strGameMode -log -AdminList=Admins -MapCycle=MapCycle -ruleset=$strRuleSet -GameStats -ApiKey=XXXXXXXXXXXXXXXXXXXXXXXX -EnableCheats -NoEAC
