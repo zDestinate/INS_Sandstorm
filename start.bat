056bd7b2ca000aacc7873cf6512beb5e16cb14ec
zWolfi <EA.ADM1N.one@gmail.com>
2019-09-10 20:38:16 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -17,6 +17,8 @@ set strMapList[12]=Compound?Scenario=Scenario_Outskirts_Checkpoint_Insurgents
 set strMapList[13]=Compound?Scenario=Scenario_Outskirts_Checkpoint_Security
 set strMapList[14]=Ministry?Scenario=Scenario_Ministry_Checkpoint_Insurgents
 set strMapList[15]=Ministry?Scenario=Scenario_Ministry_Checkpoint_Security
+set strMapList[16]=Sinjar?Scenario=Scenario_Hillside_Checkpoint_Insurgents
+set strMapList[17]=Sinjar?Scenario=Scenario_Hillside_Checkpont_Security
 
 ::Gamemode (Please pick one only and comment out the rest with :: Some will only work with specific map)
 ::set strGameMode=Firefight
@@ -27,11 +29,39 @@ set strMapList[15]=Ministry?Scenario=Scenario_Ministry_Checkpoint_Security
 ::set strGameMode=TeamDeathmatch
 ::set strGameMode=Filming
 ::set strGameMode=Mission
-set strGameMode=Checkpoint
+::set strGameMode=Checkpoint
 ::set strGameMode=CheckpointHardcore
 ::set strGameMode=CheckpointTutorial
 ::set strGameMode=Operations
 ::set strGameMode=Outpost
+
+::Mutator list
+::AllYouCanEat
+::AntiMaterielRiflesOnly
+::BoltActionsOnly
+::Broke
+::BulletSponge
+::Competitive
+::CompetitiveLoadouts
+::FastMovement
+::Frenzy
+::Guerrillas
+::Hardcore
+::HeadshotOnly
+::HotPotato
+::LockedAim
+::LowGravity
+::NoAim
+::PistolsOnly
+::ShotgunsOnly
+::SlowCaptureTimes
+::SlowMovement
+::SoldierOfFortune
+::SpecialOperations
+::Strapped
+::Ultralethal
+::Vampirism
+::Warlords
 
 ::Ruleset (Please pick one only and comment out the rest with :: Some might not work)
 ::Using Ruleset will override some of your game.ini config. Expect some config not to work
@@ -39,12 +69,12 @@ set strGameMode=Checkpoint
 ::set strRuleSet=CompetitiveFirefight
 ::set strRuleSet=CompetitiveTheater
 ::set strRuleSet=MatchmakingCasual
-set strRuleSet=OfficialRules
+::set strRuleSet=OfficialRules
 
 ::RNG to pick random map
-set /A nRng=%RANDOM% %%15
+set /A nRng=%RANDOM% %%17
 setlocal enableDelayedExpansion
 set strMap=!strMapList[%nRng%]!
 
 ::GSLTToken from https://steamcommunity.com/dev/managegameservers with App ID 581320
-Insurgency\Binaries\Win64\InsurgencyServer-Win64-Shipping.exe %strMap%?port=27102?queryport=27131?MaxPlayers=10?Game=%strGameMode% -log -AdminList=Admins -MapCycle=MapCycle -ruleset=%strRuleSet% -GameStats -GSLTToken=XXXXXXXXXXXXXXXXXXXXXXXXXXXXX -EnableCheats -NoEAC
+Insurgency\Binaries\Win64\InsurgencyServer-Win64-Shipping.exe %strMap%?port=27102?queryport=27131?MaxPlayers=10?Mutator=Hardcore,FastMovement -log -AdminList=Admins -MapCycle=MapCycle -GameStats -GSLTToken=XXXXXXXXXXXXXXXXXXXXXXXXXXXXX -EnableCheats -NoEAC
